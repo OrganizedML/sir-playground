8c50f58b4f809c94b818f5243aaac1820bbece0f
Marius <marius000steger@gmail.com>
2020-04-05 13:18:34 +0200

    potential force divide by 0 bugfix

--- sir-spa/src/model/Space.js
+++ sir-spa/src/model/Space.js
@@ -10,19 +10,22 @@ class Space {
         this.lc = [...Array(this.height)].map(x=>Array(this.width).fill(-1))  
         this.ll = [-1];
 
+        this.home_multiplier = 0.25;
+        this.center_multiplier = 0.25;
+        this.repulsion_force_multiplier = 2;
+
         // potential fields
         this.attractive_points = new Array();
 
-        this.add_attraction_at([Math.floor(this.width/2), Math.floor(this.width/2)], 0.25, "work"); // middle of grid as attractive force)
+        this.add_attraction_at([Math.floor(this.width/2), Math.floor(this.width/2)], this.home_multiplier, ["work"]); // middle of grid as attractive force)
         /*
         // removed due to schedule and time related hotspots
         this.add_attraction_at([10, this.height-10], 0.25); // up left
         this.add_attraction_at([this.width-10, this.height-10], 0.25); // up right
         this.add_attraction_at([10, 10], 0.25); // down left        
         this.add_attraction_at([this.width-10, 10], 0.25); // down right
-        */
-        
-        this.repulsion_force_multiplier = 2;
+        */       
+
         this.lc = [...Array(this.height)].map(x=>Array(this.width).fill(-1))  
         this.ll = new Array();
         this.ll[0] = [-1, -1, [0, 0]];
@@ -50,8 +53,6 @@ class Space {
         } else {
             return [];
         }
-
-
     }
 
     get_potential_force(agent, alpha=1, beta=3) {
@@ -66,34 +67,34 @@ class Space {
         for (var p of pos) {
             var dist = distance(agent.position, p);
             var U_grad_rep = this.repulsion_force_multiplier / Math.pow(dist, 2) *(1 / dist - 1/ (agent.model.repulsion_range + 0.1));
-            
-            // normalize
+
             force_x_rep += - U_grad_rep * (agent.position[0] - p[0]) / dist;
             force_y_rep += - U_grad_rep * (agent.position[1] - p[1]) / dist;
-
         }
 
         for (var att of this.attractive_points) { //-\nabla U_{att}(\mathbf{x}) = -\alpha (\mathbf{x}-\mathbf{x_{goal}}) 
             if (att[2].includes(agent.model.current_mode)) {
                 var dist = distance(agent.position, att[0]);
 
-                // normalize - later
                 force_x_att += - att[1] * (agent.position[0] - att[0][0])/ dist;
                 force_y_att += - att[1] * (agent.position[1] - att[0][1])/ dist;
             }            
         }
 
         if (force_x_att == 0 && force_y_att == 0) {
-            
             var home_pos = this.agent_list.filter(function(ag) {return ag[0] == agent.unique_id;})[0];
             var dist = distance(agent.position, home_pos[2]); // home position
             
-            // normalize 
-            force_x_att += - (agent.position[0] - home_pos[2][0])/ dist;
-            force_y_att += - (agent.position[1] - home_pos[2][1])/ dist;
+            if (dist > 0.05) {
+                force_x_att = - this.home_multiplier * (agent.position[0] - home_pos[2][0])/ dist;
+                force_y_att = - this.home_multiplier * (agent.position[1] - home_pos[2][1])/ dist;
+            }            
         }
+        // normalize  ?
+        var grad_x = alpha*force_x_att - beta*force_x_rep;
+        var grad_y = alpha*force_y_att - beta*force_y_rep;
 
-        return [alpha*force_x_att - beta*force_x_rep, alpha*force_y_att - beta*force_y_rep] // todo weight rep and att
+        return [grad_x, grad_y] // todo weight rep and att
     }
 
     reset_linked_cell(range) {
