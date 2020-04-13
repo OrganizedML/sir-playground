4ec7ec11be5cffcb5c5277e3a9741a7116d339d4
Daniel Klitzke <daniel.klitzke@renumics.com>
2020-04-13 23:59:29 +0200

    fix invalid slice

--- sir-spa/src/components/StatsDisplay.js
+++ sir-spa/src/components/StatsDisplay.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Box, Typography, Grid } from "@material-ui/core";
 
 const StatsDisplay = React.memo(({chartData=0, R0=-1}) => {
-    var sus = 0
-    var i = 0;
-    var i_all = 0;
-    var r = 0;
+    let sus = 0
+    let i = 0;
+    let i_all = 0;
+    let r = 0;
     if (chartData.datasets.length > 0) {
-        sus = chartData.datasets[2].data.slice(-1);
-        r = chartData.datasets[1].data.slice(-1);
-        i = chartData.datasets[0].data.slice(-1);
-        i_all = chartData.datasets[3].data.slice(-1);
+        sus = chartData.datasets[2].data.slice(-1)[0];
+        r = chartData.datasets[1].data.slice(-1)[0];
+        i = chartData.datasets[0].data.slice(-1)[0];
+        i_all = chartData.datasets[3].data.slice(-1)[0];
     }
   return (
     <Grid>
