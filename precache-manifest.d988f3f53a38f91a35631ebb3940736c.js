ef4c5dc01f7dd22a119b92db4e5f9e3356880edc
Daniel Klitzke <daniel.klitzke@renumics.com>
2020-04-13 15:58:43 +0200

    Updates

---
+++ precache-manifest.d988f3f53a38f91a35631ebb3940736c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4f036d1e3aa7718def6f65e079e01f",
+    "url": "/sir-playground/index.html"
+  },
+  {
+    "revision": "649196778574186ae203",
+    "url": "/sir-playground/static/css/main.3e142e2c.chunk.css"
+  },
+  {
+    "revision": "29423ff1199e3439669d",
+    "url": "/sir-playground/static/js/2.194a04d0.chunk.js"
+  },
+  {
+    "revision": "a224ffb4555a8b1614472d94edb9cd0d",
+    "url": "/sir-playground/static/js/2.194a04d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649196778574186ae203",
+    "url": "/sir-playground/static/js/main.4ae7fc11.chunk.js"
+  },
+  {
+    "revision": "859fd6cfb9164bdc7071",
+    "url": "/sir-playground/static/js/runtime-main.78c6e369.js"
+  },
+  {
+    "revision": "d40fc481f40eed3c3ae7d3f56f674203",
+    "url": "/sir-playground/static/media/icons8-business-buildings.d40fc481.svg"
+  },
+  {
+    "revision": "6a1458e187b32e4666748c93de19faff",
+    "url": "/sir-playground/static/media/icons8-manufacturing.6a1458e1.svg"
+  },
+  {
+    "revision": "4b917871311766aeb66fa417bee26e77",
+    "url": "/sir-playground/static/media/icons8-organization.4b917871.svg"
+  },
+  {
+    "revision": "9024a2d170ca403fca5b6aebc1ed6108",
+    "url": "/sir-playground/static/media/icons8-park-with-street-light.9024a2d1.svg"
+  }
+]);
