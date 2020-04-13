b748acef5f99994e7b53a803bea9c38d0a4477ab
Daniel Klitzke <daniel.klitzke@renumics.com>
2020-04-13 15:45:36 +0200

    add build

---
+++ docs/precache-manifest.6f94a304448ad301f75196692abce35b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ae3b2d82b29ce7c4c63fa90ffcdfad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24d790b17b925f88a404",
+    "url": "/static/css/main.3e142e2c.chunk.css"
+  },
+  {
+    "revision": "35cd372065946f063a9f",
+    "url": "/static/js/2.6022a228.chunk.js"
+  },
+  {
+    "revision": "a224ffb4555a8b1614472d94edb9cd0d",
+    "url": "/static/js/2.6022a228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d790b17b925f88a404",
+    "url": "/static/js/main.3b25fabd.chunk.js"
+  },
+  {
+    "revision": "971b05f2b85adf6f64a9",
+    "url": "/static/js/runtime-main.78a64dcc.js"
+  },
+  {
+    "revision": "d40fc481f40eed3c3ae7d3f56f674203",
+    "url": "/static/media/icons8-business-buildings.d40fc481.svg"
+  },
+  {
+    "revision": "6a1458e187b32e4666748c93de19faff",
+    "url": "/static/media/icons8-manufacturing.6a1458e1.svg"
+  },
+  {
+    "revision": "4b917871311766aeb66fa417bee26e77",
+    "url": "/static/media/icons8-organization.4b917871.svg"
+  },
+  {
+    "revision": "9024a2d170ca403fca5b6aebc1ed6108",
+    "url": "/static/media/icons8-park-with-street-light.9024a2d1.svg"
+  }
+]);
